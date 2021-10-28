3f755025c129bc54c4d675141b8772a9d6386551
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-10-28 21:45:55 +0200

    adding functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -l
-echo "transfering file $1 to $2?$3= at" $(date)
-pwd
-ls -l $1
+set -x
+echo "transfering file $1 with name $2 and version $3 at" $(date)
+
+filename=$(basename "$1")
+filesize=$(stat -c %s "$1")
+
+curl -v -X POST -H 'Content-Type: application/octet-stream' --data-binary "@$1" https://artifacts.stonemountain.dk/packages/$2/versions/$3/file/$filename?size=$filesize
+
 time=$(date)
 echo "Time: $time"
 echo "::set-output name=time::$time"
