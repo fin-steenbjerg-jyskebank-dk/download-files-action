b070cfc051867779057a70b1acc029b2729a682c
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-11-08 18:05:51 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ filename=$(basename "$1")
 filesize=$(stat -c %s "$1")
 
 export access_token=$(\
-    curl -X POST https://auth.stonemountain.dk/realms/house/protocol/openid-connect/token \
+    curl -v -X POST https://auth.stonemountain.dk/realms/house/protocol/openid-connect/token \
     -H 'content-type: application/x-www-form-urlencoded' \
     -d "client_id=$4" \
     -d "client_secret=$5" \
     -d "grant_type=client_credentials" | jq --raw-output '.access_token' \
 )
 
-curl --insecure -v -X POST -H 'Content-Type: application/octet-stream' -H "Content-Length: $filesize" --data-binary "@$1" https://artifacts.stonemountain.dk/packages/$2/versions/$3/file/$filename?size=$filesize -H "Authorization: Bearer $access_token"
+curl -v -X POST -H 'Content-Type: application/octet-stream' -H "Content-Length: $filesize" --data-binary "@$1" https://artifacts.stonemountain.dk/packages/$2/versions/$3/file/$filename?size=$filesize -H "Authorization: Bearer $access_token"
 
 time=$(date)
 echo "Time: $time"
