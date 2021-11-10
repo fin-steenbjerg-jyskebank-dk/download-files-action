176c8a3e2a59cecb47252d6771a42e1cac07683b
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-11-10 19:47:36 +0100

    adding some debug output

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # transfer-file-action
 
-Sample action which can download files to an url
+Sample action which can download files to an url.
+
+## Test
+
+* source ~/confidential/setup.sh
+* ./entrypoint.sh Dockerfile test v1.0 $artifact_client_id $artifact_client_secret
+ 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,25 @@
 #!/bin/sh -l
-# set -x
+#set -x
 
 echo "transfering file $1 with name $2 and version $3 at" $(date)
 
 filename=$(basename "$1")
 filesize=$(stat -c %s "$1")
+url="https://artifacts.stonemountain.dk/packages/$2/versions/$3/file/$filename?size=$filesize"
 
 access_token=$(\
-    curl -X POST 'https://auth.stonemountain.dk/realms/house/protocol/openid-connect/token' \
+    curl -s -X POST 'https://auth.stonemountain.dk/realms/house/protocol/openid-connect/token' \
     -H 'content-type: application/x-www-form-urlencoded' \
     -d "client_id=$4" \
     -d "client_secret=$5" \
     -d 'grant_type=client_credentials' | jq --raw-output '.access_token' \
 )
 
-curl -X POST -H 'Content-Type: application/octet-stream' -H "Content-Length: $filesize" --data-binary "@$1" "https://artifacts.stonemountain.dk/packages/$2/versions/$3/file/$filename?size=$filesize" -H "Authorization: Bearer $access_token"
+echo "Invoking url: $url"
+
+curl -s -o /dev/null -w "Status code=%{http_code}\n" -X POST -H 'Content-Type: application/octet-stream' -H "Content-Length: $filesize" --data-binary "@$1" $url -H "Authorization: Bearer $access_token"
 
 time=$(date)
+
 echo "Time: $time"
 echo "::set-output name=time::$time"
